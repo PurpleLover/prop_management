33db7739f62dc01d6c49e8368224f44c35460337
PurpleLover <sepan223@gmail.com>
2018-08-15 15:32:31 +0700

    DAL Project - init - may use later

---
+++ eAMS.Dal/Repository/IPropertyRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace eAMS.Dal.Repository
+{
+    public interface IPropertyRepository<T> where T: class
+    {
+        IEnumerable<T> ListProperty();
+        T ListPropertyById(int id);
+        void SaveProperty(T obj);
+        void RemoveProperty(int id);
+    }
+}
---
+++ eAMS.Dal/Repository/PropertyRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace eAMS.Dal.Repository
+{
+    public class PropertyRepository<T> : IPropertyRepository<T> where T : class
+    {
+        protected readonly DbContext context;
+
+        public PropertyRepository(DbContext _context)
+        {
+            context = _context;
+        }
+
+        public IEnumerable<T> ListProperty()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T ListPropertyById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveProperty(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveProperty(T obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
